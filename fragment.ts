eb93fae4f8b630c8dade1fe5dcec08ed78154823
Eugen Sawitzki <e.sawitzki@reply.de>
2019-05-22 12:05:27 +0200

    unknown paths result in 404

--- fragment.ts
+++ fragment.ts
@@ -6,14 +6,11 @@ const server = http.createServer((req: any, res: any) => {
   const pathname = url.parse(req.url).pathname;
   const jsHeader = { "Content-Type": "application/javascript" };
   if (pathname === "/public/bundle.js") {
-	  res.writeHead(200, jsHeader);
-	  return fs.createReadStream("./build/bundle.js").pipe(res);
+    res.writeHead(200, jsHeader);
+    return fs.createReadStream("./build/bundle.js").pipe(res);
   } else {
-	  res.writeHead(200, {
-		"Content-Type": "text/html",
-		Link: '<http://localhost:5000/public/bundle.js>; rel="fragment-script"'
-	  });
-	  return res.end("");
+    res.statusCode = 404;
+    return res.end("Not Found");
   }
 });
 
